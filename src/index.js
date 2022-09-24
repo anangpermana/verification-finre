e8611ba78a084be27aab824aeb64232921723090
Anang Permana <anang143@gmail.com>
2022-09-24 18:24:12 +0700

    update mongo connect

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const PORT = 3000
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     connectTimeoutMS: 1000,
     serverSelectionTimeoutMS: 1000}).then((result) =>console.log('connected mongodb')).catch((err) => console.log(err))
 //bodyparser
